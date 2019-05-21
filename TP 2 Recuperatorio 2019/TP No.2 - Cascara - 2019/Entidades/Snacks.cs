fae03407949a3bb96d8436a1a43123b5d3f1f7bf
Phaser <elias.m.alvarez@gmail.com>
2019-05-21 17:22:50 -0300

    Trabajo Practico 2 (Recuperatorio 2019)

---
+++ TP 2 Recuperatorio 2019/TP No.2 - Cascara - 2019/Entidades/Snacks.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Producto Snack.
+        /// </summary>
+        /// <returns>Informacion del producto en formato string</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine((string)this);
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
